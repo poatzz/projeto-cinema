4a9a984ab581311c8838c92a020487f50428b1e1
Guilherme <guijocargo@live.com>
2014-05-21 08:54:36 -0300

    Create Sala.h

---
+++ entity/header/Sala.h
@@ -0,0 +1,12 @@
+namespace entity {
+    class Sala {
+        private:
+            int capacidade;
+            int numSala;
+        
+        public:
+            int getCapacidade():
+            int getNumSala();
+            void setNumSala(int);
+    };
+};
