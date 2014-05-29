58af27e9ae0088cefc25feb17f8e93b034cd86b5
Guilherme <guijocargo@live.com>
2014-05-28 23:19:18 -0300

    Update Sala.h

--- entity/header/Sala.h
+++ entity/header/Sala.h
@@ -1,12 +1,26 @@
-namespace entity {
-    class Sala {
-        private:
-            int capacidade;
-            int numSala;
-        
-        public:
-            int getCapacidade():
-            int getNumSala();
-            void setNumSala(int);
-    };
+namespace entity
+{
+  class Sala
+  {
+    private:
+      int capacidade;
+      int numSala;
+      Situacao situacao;
+      
+    public:
+      int getCapacidade():
+      int getNumSala();
+      Situacao getSituacao();
+      void setCapacidade(int);
+      void setNumSala(int);
+      void setSituacao(Situacao);
+  };
+  
+  enum Situacao
+  {
+    DISPONIVEL,
+    MANUEQUIPAMENTO,
+    REFORMA,
+    MANUGERAL
+  };
 };
